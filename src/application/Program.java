fd89209bdd603e6033e1f86668d2a2f4acc94c0d
Augusto Faria <augustofariapereira@hotmail.com>
2025-03-22 21:52:10 -0300

    Handling exceptions and clearing screen

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,9 @@
 package application;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
@@ -13,17 +15,27 @@ public class Program {
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch(); //gera o tabuleiro
 		
-		while(true) {
-			UI.printBoard(chessMatch.getPieces()); //coloca as peças no tabuleiro
-			System.out.println();
-			System.out.println("Origem: ");
-			ChessPosition source = UI.readChessPosition(sc); //passa o scanner pro método, contendo a posição de origem
-			
-			System.out.println();
-			System.out.println("Destino: ");
-			ChessPosition target = UI.readChessPosition(sc); //passa o scanner pro método, contendo a posição de destino
-			
-			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+		while(true) { //trata as exceções e permite digitar novamente após o nextLine
+			try {
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces()); //coloca as peças no tabuleiro
+				System.out.println();
+				System.out.println("Origem: ");
+				ChessPosition source = UI.readChessPosition(sc); //passa o scanner pro método, contendo a posição de origem
+				
+				System.out.println();
+				System.out.println("Destino: ");
+				ChessPosition target = UI.readChessPosition(sc); //passa o scanner pro método, contendo a posição de destino
+				
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+				
+			}catch(ChessException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}catch(InputMismatchException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
 		}
 	}
 }
