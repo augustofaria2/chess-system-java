3ba4980dbbbfb383d35a550e5864ff4d5894ab4d
Augusto Faria <augustofariapereira@hotmail.com>
2025-03-26 11:13:58 -0300

    Knight

---
+++ src/chess/pieces/Knight.java
@@ -0,0 +1,72 @@
+package chess.pieces;
+
+import boardgame.Board;
+import boardgame.Position;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Knight extends ChessPiece{ //cavalo
+
+	public Knight(Board board, Color color) {
+		super(board, color);
+	}
+	
+	@Override
+	public String toString() {
+		return "C";
+	}
+
+	private boolean canMove(Position position) { //método para dizer se o rei pode mover pra x posição
+		ChessPiece p = (ChessPiece) getBoard().piece(position);
+		return p == null || p.getColor() != getColor(); //posição vazia ou com peça diferente da cor do rei
+	}
+	
+	@Override
+	public boolean[][] possibleMoves() { //8 possiveis L do cavalo (2,1/1,2) em todas as posições
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()]; /*cria uma matriz com as dimensões do tabuleiro.
+		em que todas posições por padrão começa com falso.*/
+		Position p = new Position(0,0);
+		
+		p.setValues(position.getRow() - 1, position.getColumn() - 2);
+		if(getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		p.setValues(position.getRow() - 2, position.getColumn() - 1); 
+		if(getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		p.setValues(position.getRow() - 2, position.getColumn() + 1); 
+		if(getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		p.setValues(position.getRow() - 1, position.getColumn() + 2); 
+		if(getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		p.setValues(position.getRow() + 1, position.getColumn() + 2); 
+		if(getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		p.setValues(position.getRow() + 2, position.getColumn() + 1); 
+		if(getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		p.setValues(position.getRow() + 2, position.getColumn() - 1); 
+		if(getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		p.setValues(position.getRow() + 1, position.getColumn() - 2); 
+		if(getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		return mat; //como se o cavalo estivesse preso, já q tudo é falso, não há movimentos possíveis
+	}
+}
